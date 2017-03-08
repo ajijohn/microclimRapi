f5014e9fb400fae93dcbf69bcc08211cce85a10f
ajijohn <aji.john@gmail.com>
2017-03-07 16:12:34 -0800

    clean-up download script

--- tests/testthat/test-downloadRequest.R
+++ tests/testthat/test-downloadRequest.R
@@ -9,28 +9,27 @@ library(ncdf4)
 context("Download request artifacts")
 test_that('artifact was downloaded',{
 
+  # key, secret and rest endpoint url
   api_token = getToken('07d4d584c04941a25e291feb8881c685','9ef6bbb24a855fbb765f3890e05592f4','localhost:3000/')
   expect_that(nchar(api_token),
               equals(167))
   ma <- microclimRapi:::MicroclimAPI$new(token = api_token,url_mc='http://localhost:3000/')
-  expect_that(typeof(ma), equals("S4"))
 
   requestId = '589ce9a4b2df447c910f7d59'
-  fileName='ALBEDO_output_interval-hourly_aggregation-inst_times-19810101-19810131_created-2017-02-09-2214.nc'
+
+  # place a request to fetch the files
+  ftch_req= ma$fetch(requestId)
 
   #output <- file(tmp <- tempfile(), open = "wb")
-  # download the request artifact
-  ncD <-  ma$download(requestId,fileName)
-  writeBin(ncD, fileName)
+  ncD <-  ma$download(requestId,ftch_req$files[[1]]$Key)
 
-    # open a NetCDF file
-  ncin <- nc_open(fileName)
-  print(ncin)
-  lon <- ncvar_get(ncin, "lon")
-  nlon <- dim(lon)
-  print(nlon)
+  #File name has request id as part of it
+  writeBin(ncD, strsplit(ftch_req$files[[1]]$Key, "/")[[1]][2])
 
-  #The dimemsions of lat/long is always two
-  expect_that(2,equals(2))
+  expect_true(file.exists(strsplit(ftch_req$files[[1]]$Key, "/")[[1]][2]))
+  # delete the temporary file..
+  file.remove( strsplit(ftch_req$files[[1]]$Key, "/")[[1]][2])
+
+  #should be in teardown to remove the file
 
 })
