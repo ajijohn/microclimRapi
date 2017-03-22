b887f71eb15d43e2502b161067c318685e2305f9
ajijohn <aji.john@gmail.com>
2017-03-22 16:31:00 -0700

    Skip the tests to be run as part of CI

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,9 @@
-library(testthat)
-library(microclimRapi)
-
-test_check("microclimRapi")
+flag <- as.logical(Sys.getenv('R_CHECK_TESTS'))
+if(!is.na(flag)) {
+  if(flag == TRUE) {
+    library('testthat')
+    test_check('microclimRapi')
+  }
+} else {
+  message('Intentionally set to FALSE, Set the system variable R_CHECK_TESTS to TRUE to run all the tests.')
+}
